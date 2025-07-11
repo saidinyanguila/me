41256dab2ebdca465de31fd088bf23477951d926
Saidi <139466730+saidinyanguila@users.noreply.github.com>
2025-07-11 23:06:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ function hideHelp() {
 // Toggle Background
 document.getElementById("btn_pfp").addEventListener('click', toggleBg);
 let container = document.getElementById("container");
-let currentBg = 1;
+let currentBg = 0;
 
 function toggleBg() {
     document.getElementById("btn_pfp").classList.remove("flash");
